2f71a27412324447b23f4591a228ad2069f7658a
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-03-09 17:54:52 -0500

    Adds functions

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin (user){
+    return user.userRole === 'ADMIN'
+}
+
+function getEmail(user){
+    return (user.firstName + '.' +user.lastName + "@codeimmersives.com").toLowerCase()
+}
+
+function getPlaylistLength(list){
+    let number = 0;
+    for (const song of list.songs){
+        number++
+    }
+    return number
+}
+
+function getHardestHomework(arr){
+    let homework = ''
+    let num = []
+        for (const work of arr){
+            num.push(work.averageScore)
+        } 
+        const i = num.indexOf(Math.min.apply(null,num))
+        if (arr[i]!==undefined){
+        homework = arr[i].name
+        } else {
+            homework = ''
+        }
+    return homework
+}
+
+function createPhonebook(name, number){
+    const phoneBook = {
+    }
+        for (let n=0;n<name.length;n++){
+        phoneBook[name[n]] = number[n]
+        }
+    console.log(phoneBook)
+    return phoneBook
+}
 
 
 
